5e50f2e389124eefcab96bf81858dd9fb1ed4cda
viswanathan11 <m.vishwanthan06@gmail.com>
2026-03-01 12:19:14 +0530

    Started Fronted

--- Backend/Router/ProductRouter.js
+++ Backend/Router/ProductRouter.js
@@ -3,10 +3,12 @@ import ensureAuthenticated from "../Middleware/EnsureAuthenticated.js";
 import userModel from "../Models/users.js";
 const router = Router();
 
+console.log("---- PRODUCT ROUTER FILE LOADED ----");
 
 router.get("/",ensureAuthenticated,async (req, res) => {
-
+  console.log("PayLoad : ",req.user);
   const user=await userModel.findOne(req.email)
+
   res.status(200).json([
     user.name,
     {
